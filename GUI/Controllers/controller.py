943580511f041e4b18976474398561dee4bdb0eb
Dani-Ideas <daniideas0@gamil.com>
2024-10-14 12:33:56 -0600

    GUI READY

--- GUI/Controllers/controller.py
+++ GUI/Controllers/controller.py
@@ -1,36 +1,81 @@
 import time
+import os
+from PIL import Image, ImageTk
 from Models.ball import Ball
 from Views.view import View
+from Models.comunicator import comunicator
 
 class Controller:
     def __init__(self, window):
-        self.window = window  
         width = 500
         height = 500
         self.view = View(window, width, height)
         self.canvas = self.view.get_canvas()
+        self.view.btn2.pack_forget()
+        self.balls = {}
 
-        # Instanciación de los objetos Ball
-        self.volley_ball = Ball(self.canvas, 0, 0, 100, 1, 1, "white")
-        self.tennis_ball = Ball(self.canvas, 0, 0, 50, 4, 3, "yellow")
-        self.basket_ball = Ball(self.canvas, 0, 0, 125, 3, 5, "orange")
-        self.bowling_ball = Ball(self.canvas, 0, 0, 75, 2, 1, "black")
-        self.test_ball = Ball(self.canvas, 10, 10, 75, 0, 1, "blue")
+        current_dir = os.path.dirname(__file__)
 
+
+        # Lista con los nombres de las imágenes
+        items = ["Granjero", "Gallina", "Lobo", "Maíz"]
+
+        # Crear instancias de Ball y añadirlas al diccionario
+        for item in items:
+            image_path = os.path.join(current_dir, '..', 'images', f'{item}.png')
+            self.balls[item] = Ball(self.canvas, 50, 300, 1, 1, image_path)
+
+        self.getLog = comunicator()
+        self.logToPrint = self.getLog.start()
+        self.index = 0
+        self.top_index = len(self.logToPrint)
+        self.view.btn.config(command=lambda: self.button_pressed(1))
+        self.view.btn2.config(command=lambda: self.button_pressed(2))
+
+        # Establecer las coordenadas objetivo para cada bola
+        self.balls["Granjero"].set_target(50, 300)
+        self.balls["Maíz"].set_target(50, 400)
+        self.balls["Lobo"].set_target(50, 200)
+        self.balls["Gallina"].set_target(50, 100)
+
+    def button_pressed(self, btn_signal):
+
+        if btn_signal == 1 and self.index < self.top_index - 1:
+            self.index += 1
+            self.checkCoordinate(self.logToPrint[self.index])
+
+            if not self.view.btn2.winfo_ismapped():
+                self.view.btn2.pack(side='left')
+        elif btn_signal == 1 and self.index < self.top_index:
+            self.view.btn.pack_forget()
+            self.view.loadImage('PntExtra.jpeg')
+        elif btn_signal == 2 and self.index > 0:
+            self.index -= 1
+            self.checkCoordinate(self.logToPrint[self.index])
+
+            if not self.view.btn.winfo_ismapped():
+                self.view.btn.pack(side='right')
+
+            if self.index == 0:
+                self.view.btn2.pack_forget()
 
     def start_animation(self):
         while True:
-            # Verificar si la ventana sigue existiendo
-            if not self.window.winfo_exists():
-                break  # Salir del bucle si la ventana ha sido cerrada
-            
-            # Mover las pelotas
-            self.volley_ball.move()
-            self.tennis_ball.move()
-            self.basket_ball.move()
-            self.bowling_ball.move()
-            self.test_ball.move()
-            
+            # Mover las pelotas (acceder al diccionario)
+            for ball in self.balls.values():
+                ball.move()
+
             # Actualizar la ventana
             self.view.canvas.update()
             time.sleep(0.01)
+    
+    def checkCoordinate(self, coordinates):
+        left_side = coordinates[0]  # Primer array (lado izquierdo)
+        right_side = coordinates[1]  # Segundo array (lado derecho)
+
+        # Recorre todas las bolas y ajusta sus coordenadas dependiendo de su posición en el log
+        for name, ball in self.balls.items():
+            if name in left_side:
+                ball.set_target(450, ball.y)  # En el lado izquierdo
+            elif name in right_side:
+                ball.set_target(50, ball.y)  # En el lado derecho
--- GUI/Views/view.py
+++ GUI/Views/view.py
@@ -6,15 +6,24 @@ class View:
     def __init__(self, window, width, height):
         self.canvas = Canvas(window, width=width, height=height)
         self.canvas.pack()
+        self.width=width
+        self.height=height
         
-        # Guardar una referencia a la imagen como un atributo de la clase
-        current_dir = os.path.dirname(__file__)
-        image_path = os.path.join(current_dir, '..', 'images', 'river.jpg')
-        image = Image.open(image_path)
-        resized_image = image.resize((width, height))
-        self.photo = ImageTk.PhotoImage(resized_image)  
+        self.loadImage('river.jpg')
 
-        self.canvas.create_image(0, 0, anchor=NW, image=self.photo)
+        self.btn = Button(window, text = '>>>') 
+        self.btn.pack(side = 'right')  
+        self.btn2 = Button(window, text = '<<<') 
+        self.btn2.pack(side = 'left')  
     
     def get_canvas(self):
         return self.canvas
+    
+    def loadImage(self,nameimage):
+        current_dir = os.path.dirname(__file__)
+        image_path = os.path.join(current_dir, '..', 'images', nameimage)
+        image = Image.open(image_path)
+        resized_image = image.resize((self.width, self.height))
+        self.photo = ImageTk.PhotoImage(resized_image)  
+
+        self.canvas.create_image(0, 0, anchor=NW, image=self.photo)
